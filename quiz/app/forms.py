35febd6a46e91547c06187095c3f7c940da5bc6d
Sam Thornbury <22232705@student.uwa.edu.au>
2020-05-09 14:32:59 +0800

    Added registration form and admin section

--- quiz/app/forms.py
+++ quiz/app/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, EqualTo, Email, Length
 
 class LoginForm(FlaskForm):
-    username=StringField('Username', validators=[InputRequired()])
-    password=PasswordField("Password", validators=[InputRequired()])
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email Address", validators=[Email()])
+    username = StringField("Enter a Username", validators=[InputRequired()]) # Need to check in database if username already exists!!!!!!
+    password = PasswordField("Enter a Password", validators=[InputRequired(), Length(min=4, message="Passwords must contain at least 4 characters ")]) #Change to a regex matching some password policy!
+    password_confirm = PasswordField("Confirm Password", validators=[InputRequired(), EqualTo('password', message="Passwords do not match! ")])   
+    submit = SubmitField("Register")
--- quiz/app/routes.py
+++ quiz/app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegisterForm
 
 @app.route('/')
 @app.route('/index')
@@ -15,3 +15,18 @@ def login():
         flash("Login requested for user {}, remember_me={}".format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', form=form)
+
+
+@app.route('/admin')
+#NEED TO ADD PROTECTIONS TO THIS URL -> NOT ALLOW ACCESS UNLESS CREDENTIALED LOGIN W/ CORRECT PERMISSIONS
+# Redirect to index if not correct user
+def admin():
+    return render_template("admin-splash.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form=RegisterForm()
+    if form.validate_on_submit():
+        flash("Account created for user {}, email= {}".format(form.username.data, form.email.data))
+        return redirect(url_for('index'))
+    return(render_template('register.html', form=form))

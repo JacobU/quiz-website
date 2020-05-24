7f5661fcdb50bcdbe3a8f0fcb7d5b59dc332cdb5
Fraser Gilbert <frasergilbert184@gmail.com>
2020-05-24 22:07:54 +0800

    touched up on bugs in routes.py with category function

--- quiz/app/routes.py
+++ quiz/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, jsonify, json, request
+from flask import render_template, flash, redirect, url_for, jsonify, json, request, session
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
 from app.forms import LoginForm, RegisterForm, AddUserForm, EditUserForm, CategoryForm, EditProfileForm
@@ -145,7 +145,7 @@ def admin_edituser():
 
 @app.route('/category', methods = ['GET', 'POST'])
 def category():
-    cat = [('Sport','Sport'), ('Music','Music'), ('Food','Food')]
+    cat = [('Sport','Sport'), ('General','General'), ('Food','Food')]
     form = CategoryForm()
     form.categories.choices = cat
     result = form.categories.data
@@ -164,17 +164,32 @@ def category():
                 "correct answers": ac
             }
             x = json.dumps(quest)
-            return x
-            quest = json.dumps({"username": str(user[0].username),"questions": q,"answers": a,})
-            
-            
-            for i in range(1,10):
-                quest = Question.query.filter_by(category=result).order_by(func.random()).limit(1)
-                q.append(str(quest.question))
+            session['request'] = x
 
-            quest = json.dumps({"username": str(user[0].username),"questions": q, }) # <--- not fully done yet.
+
+             #q = Question.query.filter_by(category=result).order_by(func.random()).limit(10)
+            z = Question.query.filter_by(category = result).order_by(func.random()).limit(5)
+            q = []
+            for i in range(0,4):
+                q[i]
+            return q
+            x = session.query(QuestionAnswer.answer_id).filter_by(question_id = "x")
+            x.filter_by(correct = True)
+
+
+            #answers = QuestionAnswer.query.filter_by(questions.get_id() = answers.get_id()) 
+
+
+            #quest = json.dumps({"username": str(user[0].username),"questions": q,"answers": a,})
+            
+            
+            #for i in range(1,10):
+                #quest = Question.query.filter_by(category=result).order_by(func.random()).limit(1)
+                #q.append(str(quest.question))
+
+            #quest = json.dumps({"username": str(user[0].username),"questions": q, }) # <--- not fully done yet.
             #answers = QuestionAnswer.query.filter_by(questions.get_id() = answers.get_id()) <--- Still needs to be done
-            return(redirect(url_for('index')))
+            return(redirect(url_for('quiz')))
         else:
             flash(result)
             return(redirect(url_for('category')))
@@ -197,7 +212,7 @@ def edit_profile():
            flash("Username already taken!")
         else:
             current_user.username = form.username.data
-            current_user.userbio = form.about_me.data
+            current_user.about_me = form.about_me.data
             db.session.commit()
             flash('Changed username to: ')
     return render_template('edit_profile.html', title='Edit Profile',
@@ -207,23 +222,27 @@ def edit_profile():
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
     
+    question_set = session.get('request')
+    return question_set
     # Get the JSON from the server...
     # quizQuestions = request.json['questions']
     
 
+   
     # THIS IS JUST USED TO LOAD A TEST JSON
-    filename = os.path.join(app.static_folder, 'new.json')
+    filename = os.path.join(app.static_folder, 'question.json')
     with open(filename) as jsonfile:
         rawdata = json.load(jsonfile)
-        
-        
+        data = rawdata['questions']
+
+    # REPLACE ABOVE WITH A DATABASE CALL
+
         questions = rawdata["questions"]
         answers = rawdata["answers"]
         corrAnswers = rawdata["correct answers"]
 
+
     return render_template("quiz.html", title="Quiz",   questions=questions,
                                                         answers=answers,
                                                         corrAnswers=corrAnswers)
-    
-
-                            
+                                                        
